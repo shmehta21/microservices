2f93ff18a3090f1be86b190f61f1701379248888
Sagar <shmehta21@gmail.com>
2023-01-05 19:55:29 +0530

    Circuit breaker functionality in User service
    Adding Circuit breaker functionality to User service

--- UserService/src/main/java/com/microservice/users/controller/UserController.java
+++ UserService/src/main/java/com/microservice/users/controller/UserController.java
@@ -2,6 +2,8 @@ package com.microservice.users.controller;
 
 import com.microservice.users.entities.User;
 import com.microservice.users.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -25,9 +28,21 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name="userToRatingAndHotelService", fallbackMethod = "userToRatingAndHotelServiceFallback")
     public  ResponseEntity<User> getSingleUser(@PathVariable String userId){
         User foundUser = userService.getUser(userId);
         return ResponseEntity.ok(foundUser);
+    }
+
+    //fallback method
+    private ResponseEntity<User> userToRatingAndHotelServiceFallback(String userId, Exception ex){
+        log.info("Fallback executed as the dependent services are down: ", ex.getMessage());
+        return new ResponseEntity<>(User.builder()
+                .email("dummy@email.com")
+                .name("dummy")
+                .about("returning dummy user")
+                .userId("dummy123")
+                .build(), HttpStatus.OK);
     }
 
     @GetMapping
